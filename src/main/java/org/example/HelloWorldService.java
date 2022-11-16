3827bcc9ce5f0a86129d061f1143bd424e8c7ec3
Blake Stickland <blakelists@yahoo.com.au>
2022-11-16 17:08:00 +1100

    can return Hello, World on localhost port 4567 with endpoint /hello

--- src/main/java/org/example/HelloWorldService.java
+++ src/main/java/org/example/HelloWorldService.java
@@ -4,11 +4,15 @@ import static spark.Spark.*;
 
 public class HelloWorldService {
     public static void main(String[] args) {
+//            After running the class HelloWorldService as a normal Java class, you will be able to
+//            access the service on its default port of 4567 using the routes defined
+//            with the get method below.
 
         get("/hello", (req, res)->"Hello World!!!");
 
         get("/hello/:name", (req, res)->{
             return "Hello, " + req.params(":name");
+
         });
     }
 }
